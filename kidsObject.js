9824b7c50d2b289ca0b23ef38f6f0ef709ce437f
amarjyoti patra <amarjyotipatra511@gmail.com>
2021-08-27 10:21:52 +0530

    latest object

---
+++ kidsObject.js
@@ -0,0 +1,98 @@
+let kids = [
+  {
+    name: "Star Print Shirt with Patch Pocket",
+    brand: "612 LEAGUE",
+    price: 839,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20201102/6ANV/5fa01f61f997dd8c83810247/-1117Wx1400H-441019691-navy-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20201102/EIzh/5fa01f61f997dd8c83810284/-1117Wx1400H-441019691-navy-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20201102/b2Ec/5fa0239cf997dd8c83812729/-1117Wx1400H-441019691-navy-MODEL3.jpg",
+    image4:
+      "https://assets.ajio.com/medias/sys_master/root/20201102/pAqw/5fa02b96f997dd8c83816c46/-1117Wx1400H-441019691-navy-MODEL4.jpg",
+  },
+  {
+    name: "Checked Shirt with Patch Pocket",
+    brand: "PEPE JEANS",
+    price: 979,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210618/w9sA/60cb99b6aeb269a9e3ed6f9a/-1117Wx1400H-460906303-darkwash-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210618/CJyM/60cb9d7baeb269a9e3ed7ee1/-1117Wx1400H-460906303-darkwash-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210618/PGu1/60cb9a2eaeb269a9e3ed7205/-1117Wx1400H-460906303-darkwash-MODEL3.jpg",
+  },
+  {
+    name: "Checked Hoodie with Patch Pocket",
+    brand: "GINI & JONY",
+    price: 839,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210209/RDmP/6022aadc7cdb8c1f143b1788/-473Wx593H-441035869-green-MODEL2.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210209/RDmP/6022aadc7cdb8c1f143b17a8/-1117Wx1400H-441035869-green-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210209/Mltb/6022a96f7cdb8c1f143b13ba/-1117Wx1400H-441035869-green-MODEL3.jpg",
+  },
+  {
+    name: "Embellished V-neck Shift Dress",
+    brand: "CUTECUMBER",
+    price: 1795,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210720/2uJ2/60f5e3d0f997ddb3122c933c/-1117Wx1400H-462643139-black-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210720/SWsZ/60f5e3d0f997ddb3122c9382/-1117Wx1400H-462643139-black-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210720/y2In/60f5e3d0f997ddb3122c937c/-1117Wx1400H-462643139-black-MODEL3.jpg",
+    image4:
+      "https://assets.ajio.com/medias/sys_master/root/20210720/veYe/60f5e3d0f997ddb3122c939f/-1117Wx1400H-462643139-black-MODEL4.jpg",
+  },
+  {
+    name: "Graphic Print Tank T-shirt",
+    brand: "RIO GIRLS",
+    price: 900,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210312/odPX/604b616daeb26969818263d8/-1117Wx1400H-441118318-white-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210312/0Kio/604b5f927cdb8c1f14600e7e/-1117Wx1400H-441118318-white-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210312/Y4A4/604b5f99aeb2696981825df5/-1117Wx1400H-441118318-white-MODEL3.jpg",
+  },
+  {
+    name: "Logo Print Zip-Front Hoodie",
+    brand: "GAP",
+    price: 1099,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210417/kz9Y/6079d932f997dd7b64b42402/-1117Wx1400H-460873407-multicolour-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210416/HZiQ/6079bcaeaeb269a9e393555f/-1117Wx1400H-460873407-multicolour-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210416/E55H/6079ae22f997dd7b64b3d4a3/-1117Wx1400H-460873407-multicolour-MODEL3.jpg",
+    image4:
+      "https://assets.ajio.com/medias/sys_master/root/20210416/Dnrq/6079af05f997dd7b64b3d7e5/-473Wx593H-460873407-multicolour-MODEL4.jpg",
+  },
+  {
+    name: "Printed Hoodie with Kangaroo Pocket",
+    brand: "KB TEAM SPIRIT",
+    price: 288,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210405/bwAd/606bdf6ff997dd7b64a15ef2/-1117Wx1400H-441116646-blue-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210405/8AwV/606bde22f997dd7b64a13e6d/-1117Wx1400H-441116646-blue-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210405/x4fc/606beab5f997dd7b64a27ec6/-1117Wx1400H-441116646-blue-MODEL3.jpg",
+    image4:
+      "https://assets.ajio.com/medias/sys_master/root/20210405/0Oqi/606bfed5f997dd7b64a446eb/-1117Wx1400H-441116646-blue-MODEL4.jpg",
+  },
+  {
+    name: "Drawstring Track Pants with Contrast Panels",
+    brand: "KB TEAM SPIRIT",
+    price: 288,
+    image1:
+      "https://assets.ajio.com/medias/sys_master/root/20210415/1nqD/6078327aaeb269a9e3924f8d/-1117Wx1400H-441118718-yellow-MODEL.jpg",
+    image2:
+      "https://assets.ajio.com/medias/sys_master/root/20210415/0Br7/60783370f997dd7b64b2dfd9/-1117Wx1400H-441118718-yellow-MODEL2.jpg",
+    image3:
+      "https://assets.ajio.com/medias/sys_master/root/20210415/o1Bd/607835adf997dd7b64b2ed2f/-1117Wx1400H-441118718-yellow-MODEL3.jpg",
+  },
+];
