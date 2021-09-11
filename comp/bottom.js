72206a28b2dc8ae3109a7045c1cb545686ebebe7
Shivang <maneyrocks@gmail.com>
2021-09-11 14:23:15 +0530

    ver2.0.0

---
+++ comp/bottom.js
@@ -0,0 +1,76 @@
+function bottom() {
+ return `    <div id="block8">
+      <div>
+        <i class="fas fa-sync-alt"></i>
+        <p>EASY EXCHANGE<sup>*</sup></p>
+      </div>
+      <div>
+        <i class="fas fa-hand-holding-heart"></i>
+        <p>100% HANDPICKED</p>
+      </div>
+      <div>
+        <i class="fas fa-check-circle"></i>
+        <p>ASSURED QUALITY</p>
+      </div>
+    </div>
+
+    <div id="fnal">
+      <div>
+        <div>
+          <div>
+            <h4>Ajio</h4>
+            <p><a href="">Who We Are</a></p>
+            <p><a href="">Join Our Team</a></p>
+            <p><a href="">Terms & Conditions</a></p>
+            <p><a href="">We Respect Your Privacy</a></p>
+            <p><a href="">Fees & Payments</a></p>
+            <p><a href="">Returns & Refunds</a></p>
+            <p><a href="">Promotions Terms & Conditions</a></p>
+          </div>
+          <div>
+            <h4>Help</h4>
+            <p><a href="">Track Your Order</a></p>
+            <p><a href="">Frequently Asked Questions</a></p>
+            <p><a href>Returns</a></p>
+            <p><a href="">Cancellation</a></p>
+            <p><a href="">Payments</a></p>
+            <p><a href="">Customer Care</a></p>
+            <p><a href="">How Do I Redeem My Coupon</a></p>
+          </div>
+          <div>
+            <h4>Shop by</h4>
+            <p><a href="">Men</a></p>
+            <p><a href="">Women</a></p>
+            <p><a href="">Kids</a></p>
+            <p><a href="">Indie</a></p>
+            <p><a href="">Stores</a></p>
+            <p><a href="">New Arrivals</a></p>
+            <p><a href="">Brand Directory</a></p>
+            <p><a href="">Home</a></p>
+            <p><a href="">Collections</a></p>
+          </div>
+          <div>
+            <h4>Follow us</h4>
+            <p><a href="">Facebook</a></p>
+            <p><a href="">Instagram</a></p>
+            <p><a href="">Twitter</a></p>
+            <p><a href="">Pinterest</a></p>
+          </div>
+        </div>
+      </div>
+      <div id="w-line"></div>
+      <div id="pay">
+        <div>
+          <div id="ICO">
+            <h5>Payment methods</h5>
+            <i class="fas fa-university"></i> <i class="fab fa-cc-visa"></i>
+            <i class="fab fa-cc-mastercard"></i>
+            <i class="fas fa-money-bill-wave"></i>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+    <div class="FRM"></div>`
+}
+export default bottom
